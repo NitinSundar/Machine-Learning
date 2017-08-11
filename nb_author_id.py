48b9ee223ea3f2efabbcf794b5c166a2163a42df
NitinSundar <ch.parasara@gmail.com>
2017-08-11 14:11:31 +0530

    detects anthor id using naive bayes
    here i calculated time for accuracy detection

---
+++ nb_author_id.py
@@ -0,0 +1,36 @@
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
+    Use a Naive Bayes Classifier to identify emails by their authors
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+import time
+import sys
+sys.path.append("../tools")
+from sklearn.naive_bayes import GaussianNB
+
+from email_preprocess import preprocess
+
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+def nb_accuracy(features_train, labels_train, features_test, labels_test):
+    clf = GaussianNB()
+
+    t0 = time.time()
+    clf.fit(features_train, labels_train)
+    print("training time:", round(time.time() - t0, 3), "s")
+
+    t0 = time.time()
+    clf.predict(features_test)
+    print("predict time:", round(time.time() - t0, 3), "s")
+
+    accuracy = clf.score(features_test, labels_test)
+    return accuracy
+
+
+print(nb_accuracy(features_train, labels_train, features_test, labels_test))
